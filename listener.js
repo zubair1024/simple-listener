c02c96822075ef9c9bc6842ef29680ffe15f1346
Zubair Ahmed <zubair1024@gmail.com>
2017-05-18 17:26:48 +0400

    testing listening

--- listener.js
+++ listener.js
@@ -43,7 +43,7 @@ net.createServer(function (sock) {
         /**
          * Write back message
          */
-        sock.write('AT+HEXMSG=0A3031320D');
+        // sock.write('AT+HEXMSG=0A3031320D');
     });
 
     /**
